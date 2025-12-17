4a48acbfc85821f27aad1c564f2a90f0ac25287f
Rajasekhar <imraj4u9@gmail.com>
2025-12-17 22:34:07 +0530

    db.php changed

--- db.php
+++ db.php
@@ -1,17 +1,17 @@
+
 <?php
-$server   = getenv("DB_SERVER");
-$database = getenv("DB_NAME");
-$username = getenv("DB_USER");
-$password = getenv("DB_PASS");
+$server = getenv("DB_SERVER") ?: "database-1.c7swcswkad0g.ap-northeast-1.rds.amazonaws.com";
+$db = getenv("DB_NAME") ?: "electronics";
+$user = getenv("DB_USER") ?: "admin";
+$pass = getenv("DB_PASS") ?: "Sonali-12398";
 
 try {
     $conn = new PDO(
-        "sqlsrv:Server=$server;Database=$database",
-        $username,
-        $password
+        "sqlsrv:Server=$server;Database=$db;Encrypt=yes;TrustServerCertificate=yes",
+        $user,
+        $pass
     );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("DB Error: " . $e->getMessage());
 }
-?>
